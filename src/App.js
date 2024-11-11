743e594a9b9863f243b45025ef2515a9f047d39e
tien122004 <tientran122004t@gmail.com>
2024-11-11 08:58:15 +0700

    todo-list&filter-product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { sculptureList } from "./assets/data.js";
 
-function App() {
+export default function App() {
+  const [index, setIndex] = useState(0)
+
+
+  function handleClick() {
+    setIndex(index + 1)
+  }
+
+  let sculpture = sculptureList[index];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
   );
 }
-
-export default App;
---
+++ src/assets/db.js
@@ -0,0 +1,94 @@
+export const dataProducts = [
+    {
+        "albumId": 1,
+        "id": 1,
+        "price": 500,
+        "title": "accusamus beatae ad facilis cum similique qui sunt",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    },
+    {
+        "albumId": 1,
+        "id": 2,
+        "price": 400,
+        "title": "reprehenderit est deserunt velit ipsam",
+        "type": "Trouser",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCEFdpF3i4hBZVGOWjiPBo670EsC6_4JEUQIQ22mj3dviDaWBOHapiAqQvJX95",
+    },
+    {
+        "albumId": 1,
+        "id": 3,
+        "price": 100,
+        "title": "officia porro iure quia iusto qui ipsa ut modi",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    },
+    {
+        "albumId": 1,
+        "id": 4,
+        "price": 501,
+        "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+        "type": "Trouser",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCEFdpF3i4hBZVGOWjiPBo670EsC6_4JEUQIQ22mj3dviDaWBOHapiAqQvJX95",
+    },
+    {
+        "albumId": 1,
+        "id": 5,
+        "price": 510,
+        "title": "natus nisi omnis corporis facere molestiae rerum in",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    },
+    {
+        "albumId": 1,
+        "id": 6,
+        "price": 520,
+        "title": "accusamus ea aliquid et amet sequi nemo",
+        "type": "Trouser",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCEFdpF3i4hBZVGOWjiPBo670EsC6_4JEUQIQ22mj3dviDaWBOHapiAqQvJX95",
+    },
+    {
+        "albumId": 1,
+        "id": 7,
+        "price": 560,
+        "title": "officia delectus consequatur vero aut veniam explicabo molestias",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    },
+    {
+        "albumId": 1,
+        "id": 8,
+        "price": 500,
+        "title": "aut porro officiis laborum odit ea laudantium corporis",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCEFdpF3i4hBZVGOWjiPBo670EsC6_4JEUQIQ22mj3dviDaWBOHapiAqQvJX95",
+    },
+    {
+        "albumId": 1,
+        "id": 9,
+        "price": 580,
+        "title": "qui eius qui autem sed",
+        "type": "Trouser",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    },
+    {
+        "albumId": 1,
+        "id": 10,
+        "price": 200,
+        "title": "beatae et provident et ut vel",
+        "type": "Shirt",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCEFdpF3i4hBZVGOWjiPBo670EsC6_4JEUQIQ22mj3dviDaWBOHapiAqQvJX95",
+    },
+    {
+        "albumId": 1,
+        "id": 11,
+        "price": 600,
+        "title": "nihil at amet non hic quia qui",
+        "type": "Trouser",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGz7HoYsnulRDQQhTjYI9ESz3Y5KwkCIi7Ow&s",
+    }
+]
+
+export const categoryProduct = ['Shirt', 'Trouser']
+
+export const categoryPrice = ['High to low', 'Low to high']
